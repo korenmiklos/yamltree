4a9cb2c25434f6762cdbc28ad446a44334474253
Miklós Koren <miklos.koren@gmail.com>
2013-02-19 23:39:17 +0100

    build a tree of python objects and access children as attributes

---
+++ test.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+import unittest as ut 
+import yamltree as module
+
+class TestURL(ut.TestCase):
+	pass
+
+class TestDictParser(ut.TestCase):
+	def test_root_node(self):
+		node = module.parse_object('root', {})
+		self.assertIsInstance(node, module.ContainerNode)
+
+	def test_container_node(self):
+		node = module.parse_object('root', dict(a=1))
+		self.assertIsInstance(node, module.ContainerNode)
+
+	def test_container_node_child(self):
+		node = module.parse_object('root', dict(a=1))
+		self.assertIsInstance(node.a, module.LiteralNode)
+
+	def test_list_node(self):
+		node = module.parse_object('root', [dict(a=1), dict(b=1)])
+		self.assertIsInstance(node, module.ContainerNode)
+
+	def test_literal_node(self):
+		node = module.parse_object('root', 'test')
+		self.assertIsInstance(node, module.LiteralNode)
+		self.assertEqual(node.get_data(), 'test')
+
+	def test_numeric_node(self):
+		node = module.parse_object('root', 5)
+		self.assertIsInstance(node, module.LiteralNode)
+		self.assertEqual(node.get_data(), '5')
+
+	def test_multiple_layers(self):
+		root = module.parse_object('root', {'a': 1, 'b': {'c': 2, 'd': 3}})
+		self.assertListEqual([root.a.get_data(), root.b.c.get_data(), root.b.d.get_data()], [u'1', u'2', u'3'])
+
+
+class TestInterface(ut.TestCase):
+	def test_numerical_name(self):
+		def callable():
+			node = module.LiteralNode('1')
+		self.assertRaises(NameError, callable)
+
+	def test_accented_name(self):
+		node = module.LiteralNode(u'árvíztűrő')
+		self.assertEqual(node.__name__, u'arvizturo')
+
+	def test_uppercase_name(self):
+		node = module.LiteralNode('Alpha')
+		self.assertEqual(node.__name__, 'alpha')
+
+	def test_space_name(self):
+		node = module.LiteralNode('A &@#$!B')
+		self.assertEqual(node.__name__, 'a_b')
+
+	def test_verbose_name(self):
+		node = module.LiteralNode(u'árvíztűrő')
+		self.assertEqual(node.__meta__['verbose_name'], u'árvíztűrő')
+
+	def test_node_returns_string(self):
+		node = module.LiteralNode('test')
+		node.set_data(5)
+		self.assertEqual(node.get_data(),str(5))
+
+	def test_get_data_and_string(self):
+		node = module.LiteralNode('test')
+		node.set_data(5)
+		self.assertEqual(node.get_data(),unicode(node))
+
+class TestExceptions(ut.TestCase):
+	def test_long_name_rejected(self):
+		def callable():		
+			node = module.LiteralNode('A'*999)
+		self.assertRaises(NameError, callable)
+
+	def test_nonunique_name_rejected(self):
+		def callable():
+			node = module.ContainerNode('test')
+			a = module.LiteralNode('a')
+			b = module.LiteralNode('a')
+			node.add_child(a)
+			node.add_child(b)
+		self.assertRaises(NameError, callable)
+
+	def test_parent_accepts_no_data(self):
+		node = module.ContainerNode('test')
+		self.assertRaises(TypeError,node.set_data,5)
+
+	def test_parent_returns_no_data(self):
+		node = module.ContainerNode('test')
+		self.assertRaises(LookupError,node.get_data)
+
+	def test_cannot_call_node_reserved_word(self):
+		RESERVED_WORDS = ['__init__','__data__',
+							'get_tree','get_data','set_data',
+							'children_as_dictionary']
+		def callable(word):
+			Node(name=word)
+		for word in RESERVED_WORDS:
+			self.assertRaises(NameError, callable, word)
+
+class TestIteration(ut.TestCase):
+	def test_node_is_iterable(self):
+		node = module.ContainerNode('test')
+		try:
+			for point in node:
+				pass
+		except TypeError:
+			self.fail('Node is not iterable.')
+
+	def test_iterates_over_nodes(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+
+		node.add_child(a)
+		node.add_child(b)
+
+		for subnode in node:
+			self.assertIsInstance(subnode,module.Node)
+
+	def test_iteration_respects_order(self):
+		pass
+
+	def test_node_returns_all_attributes(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+		node.add_child(a)
+		node.add_child(b)
+		halmaz = set([])
+		for point in node: 
+			halmaz.add(point)
+		self.assertSetEqual(set([a,b]),halmaz)
+
+	def test_iterable_does_not_return_self(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+		node.add_child(a)
+		node.add_child(b)
+		halmaz = set([])
+		for point in node: 
+			halmaz.add(point)
+		self.assertNotIn(node,halmaz)
+
+	def test_number_of_children(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+		node.add_child(a)
+		node.add_child(b)
+		self.assertEqual(len(node),2)
+
+	def test_no_child_returns(self):
+		node = module.LiteralNode('test')
+		def callable():
+			for child in node:
+				self.fail('this node should not have a child')
+		self.assertRaises(TypeError, callable)
+
+	def test_children_as_dictionary(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+		node.add_child(a)
+		node.add_child(b)
+		self.assertDictEqual(node.children_as_dictionary(),{'a': a, 'b': b})
+
+class TestLookup(ut.TestCase):
+	def test_attribute(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+
+		node.add_child(a)
+		node.add_child(b)
+
+		self.assertEqual(node.a, a)
+		self.assertEqual(node.b, b)
+
+	def test_dictionary(self):
+		node = module.ContainerNode('test')
+		a = module.LiteralNode('a')
+		b = module.LiteralNode('b')
+
+		node.add_child(a)
+		node.add_child(b)
+
+		self.assertEqual(node['a'], a)
+		self.assertEqual(node['b'], b)
+
+
+if __name__=='__main__':
+	ut.main()
---
+++ yamltree.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+'''
+
+
+__docformat__ = 'markdown en'
+__author__ = "Miklós Koren <miklos.koren@gmail.com>"
+__version__ = "0.1.0"
+
+import re
+import unidecode
+
+SLUG_REGEX = re.compile('^[A-Za-z_]\w{0,64}$')
+RESERVED_WORDS = ['get_tree', 'get_data', 'set_data', 'get_absolute_url'
+                            'children_as_dictionary']
+RESERVED_WORDS_REGEX = re.compile('^__[A-Za-z]+__$')
+
+try:
+    import unidecode
+    def slugify(verbose_name):
+        return re.sub(r'\W+','_',unidecode.unidecode(verbose_name).lower())
+except:
+    def slugify(verbose_name):
+        return re.sub(r'\W+','_',verbose_name.lower())
+
+def parse_object(name, obj):
+    '''
+    Parse a python object into a YAML tree.
+
+    >>> root = parse_object('root', {'a': 1, 'b': {'c': 2, 'd': 3}})
+    >>> root.__name__
+    'root'
+    >>> print root.a
+    1
+    >>> print root.b.c
+    2
+    >>> print root.b.d
+    3
+    >>> lst = parse_object('list', [1, 2, 3])
+    >>> print lst.id0
+    1
+    >>> print lst.id1
+    2
+    >>> print lst.id2
+    3
+
+    '''
+    if isinstance(obj, dict):
+        root = ContainerNode(name)
+        for (key, value) in obj.iteritems():
+            node = parse_object(key, value)
+            root.add_child(node)
+        return root
+    elif isinstance(obj, list):
+        root = ContainerNode(name)
+        for (key, value) in zip(range(len(obj)), obj):
+            node = parse_object('id%s' % key, value)
+            root.add_child(node)
+        return root
+    else:
+        node = LiteralNode(name)
+        node.set_data(obj)
+        return node
+
+class Node(object):
+    '''
+    Represents a YAMLTree node. Nodes can be one of three types:
+        1. list of other nodes
+        2. dictionary or other nodes
+        3. literal
+
+    Each node has a unique URL. Except for the root node, each node has a unique parent.
+    '''
+
+    def __init__(self, name, parent = None):
+        slug = slugify(name)
+        if (not SLUG_REGEX.match(slug)) or (slug in RESERVED_WORDS) or (RESERVED_WORDS_REGEX.match(slug)):
+            raise NameError, '%s is not an admissible name' % slug
+        self.__name__ = slug
+        self.__url__ = '%s/%s' % ('', name)
+        self.__meta__ = dict(verbose_name=name)
+        self.__data__ = None
+        self.__children__ = None
+
+    def get_absolute_url(self):
+        return self.__url__
+
+
+class YAMLTree(Node):
+    def __init__(self, root):
+        pass
+
+class LiteralNode(Node):
+    def get_data(self):
+        '''
+        Return data stored in child. Parents have no data.
+        '''
+        return self.__data__
+
+    def set_data(self, value):
+        '''
+        Store value directly.
+        '''
+        self.__data__ = unicode(value)
+
+    def __unicode__(self):
+        return unicode(self.get_data())
+
+    def __str__(self):
+        return self.__unicode__()
+
+class ContainerNode(Node):
+    '''
+    Children of ContainerNode can be addressed as
+
+        node['child']
+        node.child
+
+    '''
+    def __init__(self, name, parent = None):
+        super(ContainerNode, self).__init__(name, parent)
+        self.__children__ = {}
+
+    def add_child(self, node):
+        if node.__name__ in [child.__name__ for child in self]:
+            raise NameError, 'Children must have unique names'
+        self.__children__[node.__name__] = node
+
+    def __iter__(self):
+        return iter(self.__children__.values())
+
+    def __len__(self):
+        '''
+        Return number of child nodes.
+        '''
+        return len(self.__children__)
+
+    def children_as_dictionary(self):
+        return self.__children__
+
+    def __getattr__(self, name):
+        if name in self.__children__:
+            return self.__children__[name]
+        else:
+            raise KeyError
+
+    def __getitem__(self, key):
+        if key in self.__children__:
+            return self.__children__[key]
+        else:
+            raise KeyError
+
+    def set_data(self, *args):
+        raise TypeError, 'Container nodes cannot handle data directly.'
+    def get_data(self, *args):
+        raise LookupError, 'Container nodes cannot handle data directly.'
+
